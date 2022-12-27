fb23b01ea43a55b939870fd07c718590dad7374e
nilipta <niliptasatapathy@gmail.com>
2022-12-28 01:57:04 +0530

    added bootstrap to application

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { AppComponent } from './app/app.component';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
+import { provideRouter } from '@angular/router';
+import { AppRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(AppRoutes),
     importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment)),
       provideFirestore(() => getFirestore())
     ),
   ],
-});
+}).catch((err) => console.error(err));
