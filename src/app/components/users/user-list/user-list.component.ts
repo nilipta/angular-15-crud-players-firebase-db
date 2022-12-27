54389e6a1e8484c312bda92e669b349f361e71bb
nilipta <niliptasatapathy@gmail.com>
2022-12-28 01:58:55 +0530

    Connect to firebase, data successfully fetched

--- src/app/components/users/user-list/user-list.component.ts
+++ src/app/components/users/user-list/user-list.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlayersService } from 'src/app/services/players.service';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  _playersService = inject(PlayersService);
+  // constructor(private playersService: PlayersService){}
 
+  ngOnInit() {
+    this._playersService
+      .getPlayer()
+      .subscribe((res) => console.log(`The player data = `, res));
+  }
 }
