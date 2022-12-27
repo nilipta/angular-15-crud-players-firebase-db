fa5bfd9692ed8cc4bd40bb263eb87e7bee68345f
nilipta <niliptasatapathy@gmail.com>
2022-12-28 01:47:00 +0530

    added firebase firestore to application

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment)),
+      provideFirestore(() => getFirestore())
+    ),
+  ],
+});
